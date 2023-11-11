9b111d0712b739664bc2fbc705053417cb1b9a45
Aly Mohamed <aly.mohamed@ucalgary.ca>
2023-11-11 16:56:08 -0700

    more css not to be taken srsly

--- clean_to_green/src/components/about/about.jsx
+++ clean_to_green/src/components/about/about.jsx
@@ -1,45 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import "./about.css";
 const about = () => {
   return (
     <section>
-      <div className='about_div'>
+      <div className="about_div">
         <div className="about_text">
-          <h1 className='About_title'>About</h1>
-          Welcome to [Your Website Name] – Your Trusted Partner in Responsible Waste Management!
-
-At [Your Website Name], we understand that the world is facing an escalating challenge when it comes to waste management. As our population grows and consumption habits evolve, the need for efficient and sustainable garbage collection has never been more critical. This realization inspired us to create a platform dedicated to revolutionizing the way we approach waste.
-
-Who We Are:
-
-[Your Website Name] is not just a garbage collection service; we are a passionate team committed to reshaping the narrative around waste. Founded with a vision to create a cleaner, greener future, we have invested years of expertise in developing a comprehensive solution to the global waste crisis. Our team comprises industry experts, environmentalists, and technology enthusiasts united by a common goal – to make a positive impact on the planet.
-
-What We Stand For:
-
-1. **Sustainability:** We are fervently committed to sustainability. Our garbage collection practices prioritize recycling, composting, and responsible disposal to minimize environmental impact. We believe in contributing to a circular economy where waste is seen as a valuable resource.
-
-2. **Innovation:** Embracing cutting-edge technology, we've developed state-of-the-art systems to streamline garbage collection processes. Our commitment to innovation ensures that we stay at the forefront of the waste management industry, consistently seeking new and improved methods to enhance efficiency.
-
-3. **Community Engagement:** We recognize the importance of community involvement in creating lasting change. [Your Website Name] actively engages with local communities to raise awareness about waste reduction, recycling, and the overall importance of responsible waste management.
-
-4. **Transparency:** We believe in transparency in all our operations. From our collection processes to the destinations of the waste we manage, we strive to keep our customers informed. Our transparent approach builds trust and ensures that our clients are confident in our commitment to ethical waste management.
-
-What Sets Us Apart:
-
-- **Customized Solutions:** We understand that each community, business, or household has unique waste management needs. That's why we offer customizable solutions to cater to specific requirements, ensuring a tailored approach that maximizes efficiency.
-
-- **Prompt and Reliable Service:** Time is of the essence when it comes to waste management. Our dedicated team works tirelessly to provide prompt and reliable garbage collection services, ensuring that your waste is efficiently handled without causing disruptions to your routine.
-
-- **Educational Resources:** Beyond our collection services, [Your Website Name] serves as a knowledge hub for all things waste-related. Explore our blog, resources, and guides to stay informed about the latest trends in waste management and discover practical tips for reducing your ecological footprint.
-
-Join Us on the Journey:
-
-At [Your Website Name], we invite you to join us on our mission to create a world where waste is managed responsibly, sustainably, and with a profound respect for the environment. Together, we can turn the tide on the global waste crisis and pave the way for a cleaner, healthier planet. Thank you for choosing [Your Website Name] – where waste meets its responsible destination.
+          <h1 className="About_title">About</h1>
+          Welcome to [Your Website Name] – Your Trusted Partner in Responsible
+          Waste Management! At [Your Website Name], we understand that the world
+          is facing an escalating challenge when it comes to waste management.
+          As our population grows and consumption habits evolve, the need for
+          efficient and sustainable garbage collection has never been more
+          critical. This realization inspired us to create a platform dedicated
+          to revolutionizing the way we approach waste. Who We Are: [Your
+          Website Name] is not just a garbage collection service; we are a
+          passionate team committed to reshaping the narrative around waste.
+          Founded with a vision to create a cleaner, greener future, we have
+          invested years of expertise in developing a comprehensive solution to
+          the global waste crisis. Our team comprises industry experts,
+          environmentalists, and technology enthusiasts united by a common goal
+          – to make a positive impact on the planet. What We Stand For: 1.
+          **Sustainability:** We are fervently committed to sustainability. Our
+          garbage collection practices prioritize recycling, composting, and
+          responsible disposal to minimize environmental impact. We believe in
+          contributing to a circular economy where waste is seen as a valuable
+          resource. 2. **Innovation:** Embracing cutting-edge technology, we've
+          developed state-of-the-art systems to streamline garbage collection
+          processes. Our commitment to innovation ensures that we stay at the
+          forefront of the waste management industry, consistently seeking new
+          and improved methods to enhance efficiency. 3. **Community
+          Engagement:** We recognize the importance of community involvement in
+          creating lasting change. [Your Website Name] actively engages with
+          local communities to raise awareness about waste reduction, recycling,
+          and the overall importance of responsible waste management. 4.
+          **Transparency:** We believe in transparency in all our operations.
+          From our collection processes to the destinations of the waste we
+          manage, we strive to keep our customers informed. Our transparent
+          approach builds trust and ensures that our clients are confident in
+          our commitment to ethical waste management. What Sets Us Apart: -
+          **Customized Solutions:** We understand that each community, business,
+          or household has unique waste management needs. That's why we offer
+          customizable solutions to cater to specific requirements, ensuring a
+          tailored approach that maximizes efficiency. - **Prompt and Reliable
+          Service:** Time is of the essence when it comes to waste management.
+          Our dedicated team works tirelessly to provide prompt and reliable
+          garbage collection services, ensuring that your waste is efficiently
+          handled without causing disruptions to your routine. - **Educational
+          Resources:** Beyond our collection services, [Your Website Name]
+          serves as a knowledge hub for all things waste-related. Explore our
+          blog, resources, and guides to stay informed about the latest trends
+          in waste management and discover practical tips for reducing your
+          ecological footprint. Join Us on the Journey: At [Your Website Name],
+          we invite you to join us on our mission to create a world where waste
+          is managed responsibly, sustainably, and with a profound respect for
+          the environment. Together, we can turn the tide on the global waste
+          crisis and pave the way for a cleaner, healthier planet. Thank you for
+          choosing [Your Website Name] – where waste meets its responsible
+          destination.
         </div>
       </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default about
+export default about;
