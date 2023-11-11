561c925b14da47d4fa9aa78f330bc4469dd2c9b0
Apophis104 <Dawson.vanvlaanderen@ucalgary.ca>
2023-11-11 15:38:07 -0700

    started n the lambda functins, pushing to multitask withdamon

--- Backend/infra/main.tf
+++ Backend/infra/main.tf
@@ -14,15 +14,19 @@ provider "aws" {
 # the locals block is used to declare constants that 
 # you can use throughout your code
 locals {
-  function_name_SS_get_dates = "SS_get_dates"
-  function_name_SS_get_schedule = "SS_get_schedule"
-  function_name_SS_add_dates = "SS_add_dates"
-  function_name_SS_add_schedule = "SS_add_schedule"
+  function_name_contribution = "contribution"
+  function_name_donate = "donate"
+  function_name_event_get = "event_get"
+  function_name_login = "login"
+  function_name_map_data_get = "map_data_get"
+  function_name_register = "register"
   handler_name  = "main.lambda_handler"
-  artifact_name_add_schedule = "SS_add_schedule_artifact.zip"
-  artifact_name_add = "SS_add_artifact.zip"
-  artifact_name_get = "SS_get_artifact.zip"
-  artifact_name_get_schedule = "SS_get_artifact.zip"
+  artifact_name_contribution = "contribution_artifact.zip"
+  artifact_name_donate = "donate_artifact.zip"
+  artifact_name_event_get = "event_get_artifact.zip"
+  artifact_name_login = "login_artifact.zip"
+  artifact_name_map_data_get = "map_data_get_artifact.zip"
+  artifact_name_register = "register_artifact.zip"
 }
 
 # # create archive file from main.py
@@ -185,25 +189,25 @@ resource "aws_dynamodb_table" "CLEAN_TO_GREEN_USER_LOGIN_INFO" {
 
   # we only need a student id to find an item in the table; therefore, we 
   # don't need a sort key here
-  hash_key  = "User_name"
+  hash_key  = "User_email"
   range_key  = "User_pass"
 
   # the hash_key data type is string
 
-  attribute {
-    name = "User_name"
-    type = "S"
-  }
+  # attribute {
+  #   name = "User_name"
+  #   type = "S"
+  # }
 
   attribute {
     name = "User_pass"
     type = "S"
   }
 
-  # attribute {
-  #   name = "User_email"
-  #   type = "S"
-  # }
+  attribute {
+    name = "User_email"
+    type = "S"
+  }
 
   # attribute {
   #   name = "User_phone"
@@ -211,7 +215,7 @@ resource "aws_dynamodb_table" "CLEAN_TO_GREEN_USER_LOGIN_INFO" {
   # }
 }
 
-# Dynamodb table for storing login info
+# Dynamodb table for storing donors info
 # read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
 resource "aws_dynamodb_table" "CLEAN_TO_GREEN_DONOR_INFO" {
   name         = "CLEAN_TO_GREEN_DONOR_INFO"
@@ -240,7 +244,7 @@ resource "aws_dynamodb_table" "CLEAN_TO_GREEN_DONOR_INFO" {
   # }
 }
 
-# Dynamodb table for storing login info
+# Dynamodb table for storing location info
 # read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
 resource "aws_dynamodb_table" "CLEAN_TO_GREEN_LOCATION_INFO" {
   name         = "CLEAN_TO_GREEN_LOCATION_INFO"
@@ -267,4 +271,43 @@ resource "aws_dynamodb_table" "CLEAN_TO_GREEN_LOCATION_INFO" {
   #   name = "Garbage_amt"
   #   type = "N"
   # }
+
+  # attribute {
+  #   name = "Input_num"
+  #   type = "N"
+  # }
+}
+
+# Dynamodb table for storing event info
+# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+resource "aws_dynamodb_table" "CLEAN_TO_GREEN_EVENT_INFO" {
+  name         = "CLEAN_TO_GREEN_EVENT_INFO"
+  billing_mode = "PROVISIONED"
+
+  # up to 8KB read per second (eventually consistent)
+  read_capacity = 1
+
+  # up to 1KB per second
+  write_capacity = 1
+
+  # we only need a student id to find an item in the table; therefore, we 
+  # don't need a sort key here
+  hash_key  = "Location"
+
+  # the hash_key data type is string
+
+  attribute {
+    name = "Location"
+    type = "S"
+  }
+
+  attribute {
+    name = "Garbage_amt"
+    type = "N"
+  }
+
+  attribute {
+    name = "Date"
+    type = "s"
+  }
 }
