5b551bbb67d79c375f3004d9c73d5011e0aeee2d
Nicholas Garcia <nicholasgarciaaa@gmail.com>
2023-11-11 16:47:32 -0700

    added get request api

--- clean_to_green/src/components/map/map.jsx
+++ clean_to_green/src/components/map/map.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 import './map.css';
+
+
+const [data, setData] = useState([]);
+
+const getData = async () => {
+  const res = await fetch (
+    `put get api here`,
+    {
+      method: "GET",
+    }
+  );
+
+  if(res.status === 200){
+    // change this to match aws db
+    const backendData = await res.json()
+    setData(backendData["data"])
+    console.log(backendData["data"])
+  }
+}
+
+
 
 const map = () => {
   return (
     <div id="map-flex">
         <iframe id="map" allow="geolocation" src="https://data.calgary.ca/dataset/Community-Boundaries/ab7m-fwn6/embed?width=800&height=600"></iframe>
+        {/* add drop down menu of location data; do this if mapbox doesnt work */}
     </div>
   )
 }
