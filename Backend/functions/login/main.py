dec755727ac0ed359793caa06cb83b4fca456fe9
uCalgary-Damon <damon.mazurek@ucalgary.ca>
2023-11-11 21:28:55 -0700

    login working

--- Backend/functions/login/main.py
+++ Backend/functions/login/main.py
@@ -1,28 +1,46 @@
 import boto3
 import json
-from urllib import parse, request
+from boto3.dynamodb.conditions import Key
 
-dynamodb_resource = boto3.resource('dynamodb')
-table = dynamodb_resource.Table('Scheduling-Simple')
-
+dynamodb_resource = boto3.resource("dynamodb")
+table = dynamodb_resource.Table('CLEAN_TO_GREEN_USER_LOGIN_INFO')
 
 def lambda_handler(event, context):
+    # Receive and decode data
+    parts = event
 
-    body = json.loads(event['body'])
-    print(body);
-    try:
-        table.put_item(
-            Item=body
-        )
+    # Initialize variables with decoded data sent from frontend
+    User_email = parts['User_email']
+    User_pass = parts['User_pass']
+
+    # Look for email and password in DB
+    response = table.query(
+    KeyConditionExpression=Key('User_email').eq(User_email) & Key('User_pass').eq(User_pass),
+    ProjectionExpression='#n, #l, #p',  # Using placeholders for attribute names
+    ExpressionAttributeNames={
+        '#n': 'User_name',
+        '#l': 'Location',
+        '#p': 'User_phone'
+    }
+)
+
+    # Check if the user is found in the database
+    if response['Count'] != 0:
+        # Assuming there is only one item matching the email and password
+        user_data = response['Items'][0]  # Extracting the first (and only) result
+        print(user_data['User_name'])
         return {
-            'statusCode': 201,
-            'body': "success"
+            "statusCode": 200,
+            "body": json.dumps({
+                "User_name": user_data['User_name'],
+                # "Location": user_data['Location'],
+                # "User_phone": user_data['User_phone']
+            })
         }
 
-    except Exception as e:
+    # If user not found, return appropriate message
+    else:
         return {
-            'statusCode': 500,
-            'body': json.dumps({
-                "message": str(e)
-            })
+            "statusCode": 200,
+            "body": json.dumps({"message": "0"})
         }
