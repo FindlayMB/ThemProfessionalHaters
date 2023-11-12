7d225b823a53800ba97ef601afa0c676c3ef1d14
Apophis104 <Dawson.vanvlaanderen@ucalgary.ca>
2023-11-11 19:42:45 -0700

    Got the lambda function for map_data_getter done

--- Backend/functions/map_data_get/main.py
+++ Backend/functions/map_data_get/main.py
@@ -1,28 +1,53 @@
 import boto3
 import json
-from urllib import parse, request
+from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb_resource = boto3.resource('dynamodb')
-table = dynamodb_resource.Table('Scheduling-Simple')
-
+dynamodb_resource = boto3.resource('dynamodb', region_name="ca-central-1")
+table = dynamodb_resource.Table('CLEAN_TO_GREEN_LOCATION_INFO')
 
 def lambda_handler(event, context):
-
-    body = json.loads(event['body'])
-    print(body);
     try:
-        table.put_item(
-            Item=body
-        )
-        return {
-            'statusCode': 201,
-            'body': "success"
-        }
+        res = table.scan(ProjectionExpression="#loc, Garbage_sum, Input_num", ExpressionAttributeNames={"#loc": "Location"})['Items']
 
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({
-                "message": str(e)
+        transformed_data = []
+
+        for item in res:
+            # Convert values to numbers
+            garbage_sum = float(item['Garbage_sum'])
+            input_num = float(item['Input_num'])
+
+            # Calculate Garbage_val
+            if input_num == 0:
+                garbage_val=0
+            else:
+                garbage_val = (garbage_sum / input_num)/10
+
+            # Create a new dictionary with the desired structure
+            transformed_item = {
+                'Location': item['Location'],
+                'Garbage_val': garbage_val
+            }
+
+            # Append the transformed item to the list
+            transformed_data.append(transformed_item)
+        print(transformed_data)
+
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                'message': "Success",
+                'data': transformed_data
             })
         }
+        return response
+
+    except Exception as e:
+        response = {
+            "statusCode": 404,
+            "body": json.dumps(
+                {
+                    'message': str(e)
+                }
+            )
+        }
+        return response
--- Backend/infra/main.tf
+++ Backend/infra/main.tf
@@ -176,9 +176,6 @@ resource "aws_lambda_function" "map_data_get" {
   # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
   runtime = "python3.9"
 }
-  # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
-  runtime = "python3.9"
-}
 
 # create a Function URL for Lambda 
 # see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function_url
@@ -643,150 +640,150 @@ resource "aws_iam_role_policy_attachment" "lambda_logs_map_data_get" {
 
 
 
-# Dynamodb table for storing login info
-# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
-resource "aws_dynamodb_table" "CLEAN_TO_GREEN_USER_LOGIN_INFO" {
-  name         = "CLEAN_TO_GREEN_USER_LOGIN_INFO"
-  billing_mode = "PROVISIONED"
+# # Dynamodb table for storing login info
+# # read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+# resource "aws_dynamodb_table" "CLEAN_TO_GREEN_USER_LOGIN_INFO" {
+#   name         = "CLEAN_TO_GREEN_USER_LOGIN_INFO"
+#   billing_mode = "PROVISIONED"
 
-  # up to 8KB read per second (eventually consistent)
-  read_capacity = 1
+#   # up to 8KB read per second (eventually consistent)
+#   read_capacity = 1
 
-  # up to 1KB per second
-  write_capacity = 1
+#   # up to 1KB per second
+#   write_capacity = 1
 
-  # we only need a student id to find an item in the table; therefore, we 
-  # don't need a sort key here
-  hash_key  = "User_email"
-  range_key  = "User_pass"
+#   # we only need a student id to find an item in the table; therefore, we 
+#   # don't need a sort key here
+#   hash_key  = "User_email"
+#   range_key  = "User_pass"
 
-  # the hash_key data type is string
+#   # the hash_key data type is string
 
-  # attribute {
-  #   name = "User_name"
-  #   type = "S"
-  # }
+#   # attribute {
+#   #   name = "User_name"
+#   #   type = "S"
+#   # }
 
-  attribute {
-    name = "User_pass"
-    type = "S"
-  }
+#   attribute {
+#     name = "User_pass"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "User_email"
-    type = "S"
-  }
+#   attribute {
+#     name = "User_email"
+#     type = "S"
+#   }
 
-  # attribute {
-  #   name = "User_phone"
-  #   type = "S"
-  # }
-}
+#   # attribute {
+#   #   name = "User_phone"
+#   #   type = "S"
+#   # }
+# }
 
-# Dynamodb table for storing donors info
-# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
-resource "aws_dynamodb_table" "CLEAN_TO_GREEN_DONOR_INFO" {
-  name         = "CLEAN_TO_GREEN_DONOR_INFO"
-  billing_mode = "PROVISIONED"
+# # Dynamodb table for storing donors info
+# # read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+# resource "aws_dynamodb_table" "CLEAN_TO_GREEN_DONOR_INFO" {
+#   name         = "CLEAN_TO_GREEN_DONOR_INFO"
+#   billing_mode = "PROVISIONED"
 
-  # up to 8KB read per second (eventually consistent)
-  read_capacity = 1
+#   # up to 8KB read per second (eventually consistent)
+#   read_capacity = 1
 
-  # up to 1KB per second
-  write_capacity = 1
+#   # up to 1KB per second
+#   write_capacity = 1
 
-  # we only need a student id to find an item in the table; therefore, we 
-  # don't need a sort key here
-  hash_key  = "Donor_name"
+#   # we only need a student id to find an item in the table; therefore, we 
+#   # don't need a sort key here
+#   hash_key  = "Donor_name"
 
-  # the hash_key data type is string
+#   # the hash_key data type is string
 
-  attribute {
-    name = "Donor_name"
-    type = "S"
-  }
+#   attribute {
+#     name = "Donor_name"
+#     type = "S"
+#   }
 
-  # attribute {
-  #   name = "Donated_amt"
-  #   type = "N"
-  # }
-}
+#   # attribute {
+#   #   name = "Donated_amt"
+#   #   type = "N"
+#   # }
+# }
 
-# Dynamodb table for storing location info
-# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
-resource "aws_dynamodb_table" "CLEAN_TO_GREEN_LOCATION_INFO" {
-  name         = "CLEAN_TO_GREEN_LOCATION_INFO"
-  billing_mode = "PROVISIONED"
+# # Dynamodb table for storing location info
+# # read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+# resource "aws_dynamodb_table" "CLEAN_TO_GREEN_LOCATION_INFO" {
+#   name         = "CLEAN_TO_GREEN_LOCATION_INFO"
+#   billing_mode = "PROVISIONED"
 
-  # up to 8KB read per second (eventually consistent)
-  read_capacity = 1
+#   # up to 8KB read per second (eventually consistent)
+#   read_capacity = 1
 
-  # up to 1KB per second
-  write_capacity = 1
+#   # up to 1KB per second
+#   write_capacity = 1
 
-  # we only need a student id to find an item in the table; therefore, we 
-  # don't need a sort key here
-  hash_key  = "Location_code"
-  range_key  = "Location"
+#   # we only need a student id to find an item in the table; therefore, we 
+#   # don't need a sort key here
+#   hash_key  = "Location_code"
+#   range_key  = "Location"
 
-  # the hash_key data type is string
+#   # the hash_key data type is string
 
-  attribute {
-    name = "Location_code"
-    type = "S"
-  }
+#   attribute {
+#     name = "Location_code"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "Location"
-    type = "S"
-  }
+#   attribute {
+#     name = "Location"
+#     type = "S"
+#   }
 
-  # attribute {
-  #   name = "Garbage_sum"
-  #   type = "N"
-  # }
+#   # attribute {
+#   #   name = "Sector"
+#   #   type = "N"
+#   # }
+# }
 
-  # attribute {
-  #   name = "Input_num"
-  #   type = "N"
-  # }
+#   # attribute {
+#   #   name = "Garbage_sum"
+#   #   type = "N"
+#   # }
 
-  # attribute {
-  #   name = "Sector"
-  #   type = "N"
-  # }
-}
+#   # attribute {
+#   #   name = "Input_num"
+#   #   type = "N"
+#   # }
 
-# Dynamodb table for storing event info
-# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
-resource "aws_dynamodb_table" "CLEAN_TO_GREEN_EVENT_INFO" {
-  name         = "CLEAN_TO_GREEN_EVENT_INFO"
-  billing_mode = "PROVISIONED"
+# # Dynamodb table for storing event info
+# # read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+# resource "aws_dynamodb_table" "CLEAN_TO_GREEN_EVENT_INFO" {
+#   name         = "CLEAN_TO_GREEN_EVENT_INFO"
+#   billing_mode = "PROVISIONED"
 
-  # up to 8KB read per second (eventually consistent)
-  read_capacity = 1
+#   # up to 8KB read per second (eventually consistent)
+#   read_capacity = 1
 
-  # up to 1KB per second
-  write_capacity = 1
+#   # up to 1KB per second
+#   write_capacity = 1
 
-  # we only need a student id to find an item in the table; therefore, we 
-  # don't need a sort key here
-  hash_key  = "Location"
+#   # we only need a student id to find an item in the table; therefore, we 
+#   # don't need a sort key here
+#   hash_key  = "Location"
 
-  # the hash_key data type is string
+#   # the hash_key data type is string
 
-  attribute {
-    name = "Location"
-    type = "S"
-  }
+#   attribute {
+#     name = "Location"
+#     type = "S"
+#   }
 
-  # attribute {
-  #   name = "Garbage_amt"
-  #   type = "N"
-  # }
+#   # attribute {
+#   #   name = "Garbage_amt"
+#   #   type = "N"
+#   # }
 
-  # attribute {
-  #   name = "Date"
-  #   type = "S"
-  # }
-}
+#   # attribute {
+#   #   name = "Date"
+#   #   type = "S"
+#   # }
+# }
