49d7278b6c1e8fc6dc5eeffb61994c413e361132
FindlayMB <findlay.brown@ucalgary.ca>
2023-11-11 21:05:33 -0700

    Implemented Mapbox API in map page
    Added working mapbox api that is able to use popups

--- clean_to_green/src/components/map/map.jsx
+++ clean_to_green/src/components/map/map.jsx
@@ -1,35 +1,135 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+// import { ReactDOM } from 'react-dom';
+// import { ReactDOM } from 'react';
+import ReactDOM from "react-dom/client";
 import './map.css';
 
+import mapboxgl from 'mapbox-gl'
+// import Popup from "./popup";
 
-const [data, setData] = useState([]);
+mapboxgl.accessToken = 'pk.eyJ1IjoiZi1icm93biIsImEiOiJjbG91bzZrNDMwaGZmMmpucnJobTZkZWJpIn0.g-F2iMN4W7Q1GYz21tIeAQ';//'sk.eyJ1IjoiZi1icm93biIsImEiOiJjbG91dGZvamowZWQwMmlsM3R4eG1yMjNpIn0.vCOfKM6p9CLM7vorW3jd2w';
+// Create a new map.
+// const map = new mapboxgl.Map({
+//   container: 'map',
+//   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+//   style: 'mapbox://styles/f-brown/clouov3f000gj01qdava0736f',
+//   center: [-114.064692, 51.023736],
+//   zoom: 10.52
+// });
 
-const getData = async () => {
-  const res = await fetch (
-    `put get api here`,
-    {
-      method: "GET",
-    }
-  );
+// map.on('load', () => {
+//   // Add a source for the state polygons.
+//   map.addSource('states', {
+//   'type': 'geojson',
+//   'data': 'https://data.calgary.ca/resource/surr-xmvs.geojson'
+// });
 
-  if(res.status === 200){
-    // change this to match aws db
-    const backendData = await res.json()
-    setData(backendData["data"])
-    console.log(backendData["data"])
-  }
-}
+// Add a layer showing the state polygons.
+// map.addLayer({
+//   'id': 'states-layer',
+//   'type': 'fill',
+//   'source': 'states',
+//   'paint': {
+//   'fill-color': 'rgba(200, 100, 240, 0.4)',
+//   'fill-outline-color': 'rgba(200, 100, 240, 1)'
+//   }
+// });
+
+// When a click event occurs on a feature in the states layer,
+// open a popup at the location of the click, with description
+// HTML from the click event's properties.
+// map.on('click', 'states-layer', (e) => {
+//   new mapboxgl.Popup()
+//   .setLngLat(e.lngLat)
+//   .setHTML(e.features[0].properties.name)
+//   .addTo(map);
+// });
+
+// Change the cursor to a pointer when
+// the mouse is over the states layer.
+// map.on('mouseenter', 'states-layer', () => {
+//   map.getCanvas().style.cursor = 'pointer';
+// });
+
+// // Change the cursor back to a pointer
+// // when it leaves the states layer.
+// map.on('mouseleave', 'states-layer', () => {
+//   map.getCanvas().style.cursor = '';
+//   });
+// });
+
+const Popup = ({comm_code, comm_name, sector}) => (
+  <div className='popup'>
+      <h1>{comm_code}</h1>
+      <h2>{comm_name}</h2>
+      <h2>{sector}</h2>
+  </div>
+);
 
 
 
-const map = () => {
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const [lng, setLng] = useState(-114.0642);
+  const [lat, setLat] = useState(51.0235);
+  const [zoom, setZoom] = useState(10.13);
+  // const popupRef = useRef(new mapboxgl.Popup({offset: 15}));
+
+  useEffect(() => {
+    // if (map.current) return; // initialize map only once
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/f-brown/clouov3f000gj01qdava0736f',
+      center: [lng, lat],
+      zoom: zoom
+    });
+    
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+
+    map.on("click", e => {
+      const features = map.queryRenderedFeatures(e.point, {
+        layers: ["community"]
+      })
+      if (!features.length) {
+        return;
+      }
+      const feature = features[0]
+      // const popupNode = document.createElement("div")
+
+      // ReactDOM.render(
+        // <Popup
+        //     comm_code={feature["properties"]}
+        //     comm_name={feature["properties"]}
+        //     sector={feature["properties"]}
+        // />
+      // )
+      console.log(feature.geometry.coordinates[0])
+      const popup = new mapboxgl.Popup({offset: [0, -15]})
+        .setLngLat(feature.geometry.coordinates[0][0])
+        .setHTML(
+          `<div className='popup'><h1>${feature.properties.comm_code}</h1><h2>${feature.properties.name}</h2><h2>${feature.properties.sector}</h2></div>`
+        )
+        .addTo(map)
+      }
+    );
+
+    // return () => map.remove()
+  }, []);
+
+
   return (
-    <div id="map-flex">
-        <iframe id="map" allow="geolocation" src="https://data.calgary.ca/dataset/Community-Boundaries/ab7m-fwn6/embed?width=800&height=600"></iframe>
-        {/* add drop down menu of location data; do this if mapbox doesnt work */}
+    <div>
+      {/* <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div> */}
+      <div ref={mapContainer} className="map-container" />
     </div>
   )
 }
 
-export default map
+
--- clean_to_green/src/index.js
+++ clean_to_green/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "mapbox-gl/dist/mapbox-gl.css";
 import { BrowserRouter } from "react-router-dom";
 // import reportWebVitals from './reportWebVitals';
 
